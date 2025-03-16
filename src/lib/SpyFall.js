6314940cb7e70be40e42136f9d08d3898e2b3a22
kbparagua <kb.paragua@gmail.com>
2025-03-16 10:23:53 +0800

    Generate location and role

---
+++ src/lib/SpyFall.js
@@ -0,0 +1,74 @@
+import { Randomizer } from "./Randomizer";
+
+const LOCATIONS = [
+  {
+    name: 'Airplane',
+    roles: [
+      'Pilot',
+      'Co-Pilot',
+      'Stewardess',
+      'First Class Passenger'
+    ]
+  },
+  {
+    name: 'Bank',
+    roles: [
+      'Manager',
+      'Security Guard',
+      'Teller',
+      'Customer'
+    ]
+  },
+  {
+    name: 'Casino',
+    roles: [
+      'Bouncer',
+      'Dealer',
+      'Gambler',
+      'Manager'
+    ]
+  },
+  {
+    name: 'Hospital',
+    roles: [
+      'Doctor',
+      'Nurse',
+      'Patient',
+      'Intern'
+    ]
+  },
+  {
+    name: 'Police Station',
+    roles: [
+      'Police',
+      'Detective',
+      'Lawyer',
+      'Journalist'
+    ]
+  }
+];
+
+export default class {
+  constructor({ seed, playerNumber }) {
+    this.randomizer = new Randomizer(seed);
+    this.playerNumber = playerNumber;
+  }
+
+  location() {
+    if (this._location) return this._location;
+
+    const index = this.randomizer.get(0, LOCATIONS.length - 1);
+    this._location = LOCATIONS[index];
+
+    return this._location;
+  }
+
+  role() {
+    if (this._role) return this._role;
+
+    const shuffled = this.randomizer.shuffle(this.location().roles);
+    this._role = shuffled[this.playerNumber - 1];
+
+    return this._role;
+  }
+}
--- src/views/spyfall/main/GameScreen.vue
+++ src/views/spyfall/main/GameScreen.vue
@@ -1,12 +1,21 @@
 <script setup>
   import { useRoute } from 'vue-router';
+  import SpyFall from '@/lib/SpyFall';
 
   const route = useRoute();
   const player = route.query.p;
+  const seed = route.query.s;
+  const playerNumber = route.query.n;
+
+  const spyfall = new SpyFall({ seed, playerNumber });
+  const location = spyfall.location();
+  const role = spyfall.role();
 </script>
 
 <template>
   <div>
     <h1>{{ player }}</h1>
+    <div>Location: {{ location.name }}</div>
+    <div>Role: {{ role  }}</div>
   </div>
 </template>
