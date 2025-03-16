a30fa04d5dbfd4f579ee7f0dcb380c467d7a0414
kbparagua <kb.paragua@gmail.com>
2025-03-16 10:59:11 +0800

    Use v-for to render PlayerFields

--- src/views/spyfall/main/NewGameScreen.vue
+++ src/views/spyfall/main/NewGameScreen.vue
@@ -1,11 +1,22 @@
 <script setup>
 
+import { reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import PlayerField from "./new_game_screen/PlayerField.vue";
 
 const router = useRouter();
-const players = [];
-const setPlayer = (index, name) => {
+const players = reactive([
+  { name: '' },
+  { name: '' },
+  { name: '' }
+]);
+
+function buildLabel(playerIndex) {
+  if (playerIndex == 0) return 'Host';
+  return `Player ${playerIndex + 1}`;
+}
+
+function setPlayer(index, name) {
   players[index] = name;
 }
 
@@ -13,14 +24,12 @@ function create() {
   const seed = Math.round((new Date()).getTime() * Math.random()).toString();
   router.push({ path: 'lobby', query: { players, seed } });
 }
-
 </script>
 
 <template>
   <div>Spyfall</div>
-  <PlayerField label="Host" @change="(v) => setPlayer(0, v)"></PlayerField>
-  <PlayerField label="Player 2" @change="(v) => setPlayer(1, v)"></PlayerField>
-  <PlayerField label="Player 3" @change="(v) => setPlayer(2, v)"></PlayerField>
-  <PlayerField label="Player 4" @change="(v) => setPlayer(3, v)"></PlayerField>
+  <template v-for="(player, i) in players">
+    <PlayerField :label="buildLabel(i)" @change="(value) => setPlayer(i, value)"></PlayerField>
+  </template>
   <button @click="create">Create</button>
 </template>
