5d8bd302c3ae80c4895bc9d04584ab64a6392cea
kbparagua <kb.paragua@gmail.com>
2025-03-17 21:49:33 +0800

    Resume and Pause timer

--- src/views/spyfall/main/game_screen/Timer.vue
+++ src/views/spyfall/main/game_screen/Timer.vue
@@ -1,16 +1,34 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
   const DEFAULT_MINUTES = 8;
+  const TICK_INTERVAL_MS = 1000;
 
+  const intervalId = ref(null);
   let seconds = 0;
   let minutes = DEFAULT_MINUTES;
   const secondsDisplay = ref(formatSeconds());
   const minutesDisplay = ref(minutes.toString());
+  
 
   function formatSeconds() {
     return (seconds < 10) ? `0${seconds.toString()}` : seconds.toString();
   }
+
+  function startTick() {
+    intervalId.value = setInterval(tick, TICK_INTERVAL_MS);
+  }
+
+  function stopTick() {
+    clearInterval(intervalId.value);
+    intervalId.value = null;
+  }
+
+  function toggleTick() {
+    intervalId.value ? stopTick() : startTick();
+  }
+
+  const toggleAction = computed(() => (intervalId.value ? 'Pause' : 'Resume' ));
 
   function tick() {
     seconds--;
@@ -24,13 +42,10 @@ import { ref, onMounted } from 'vue';
     minutesDisplay.value = minutes.toString();
   }
 
-  onMounted(() => {
-    setInterval(() => {
-      tick();
-    }, 1000);
-  }); 
+  onMounted(() => startTick());
 </script>
 
 <template>
   {{ minutesDisplay }}:{{ secondsDisplay }}
+  <a href="#toggleTick" @click.prevent="toggleTick">{{ toggleAction }}</a>
 </template>
