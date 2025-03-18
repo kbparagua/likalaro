6a19c50a4fd72d9215a191450779cd748e1efc69
kbparagua <kb.paragua@gmail.com>
2025-03-18 21:55:23 +0800

    copy to clipboard

--- src/views/spyfall/main/Lobby.vue
+++ src/views/spyfall/main/Lobby.vue
@@ -1,5 +1,7 @@
 <script setup>
+  import { onMounted, onUnmounted } from 'vue';
   import { useRoute, useRouter } from 'vue-router';
+  import Clipboard from 'clipboard';
 
   const router = useRouter();
   const route = useRoute();
@@ -7,6 +9,7 @@
   const { players, seed } = route.query;
   const host = players.shift();
   const numberOfPlayers = players.length + 1;
+  let clipboard = null;
 
   function generateJoinUrl({ index, player }) {
     const data = {
@@ -27,13 +30,21 @@
   function start() {
     router.push({ path: '/spyfall/game', query: { seed, numberOfPlayers, index: 0, player: host} });
   }
+
+  onMounted(() => {
+    clipboard = new Clipboard('.copy-to-clipboard')
+  });
+  
+  onUnmounted(() => {
+    clipboard.destroy();
+  });
 </script>
 
 <template>
   <div>Host: {{  host  }}</div>
   <ul>
     <li v-for="(player, i) in players">
-      {{ player }}: {{ joinUrls[i] }}
+      {{ player }}: {{ joinUrls[i] }} <button class="copy-to-clipboard" :data-clipboard-text="joinUrls[i]">Copy</button>
     </li>
   </ul>
 
